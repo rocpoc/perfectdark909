b0f9c6e8c959cdfbb74094969039ec8e3bc41df3
rocky lubbers <rocky.lubbers@gmail.com>
2023-02-26 22:35:08 -0800

    add music site

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Music />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
         <Route path="music" element={<Music />} />
