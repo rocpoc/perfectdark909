c63595646c90d89f7bb66ccfd1363b39d29c3e83
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2023-03-05 13:28:31 -0800

    Add fade in

--- src/components/Discography.tsx
+++ src/components/Discography.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import MusicElement from "./MusicElement";
 
 const releases = [
   {
@@ -44,50 +44,10 @@ const releases = [
 ];
 
 const Discography = () => {
-  useEffect(() => {
-    // Callback for IntersectionObserver
-    const callback = function (entries: IntersectionObserverEntry[]) {
-      entries.forEach((entry) => {
-        // Is the element in the viewport?
-        if (entry.isIntersecting) {
-          // Add the fadeIn class:
-          entry.target.classList.add("motion-safe:animate-fadeIn");
-        } else {
-          // Otherwise remove the fadein class
-          entry.target.classList.remove("motion-safe:animate-fadeIn");
-        }
-      });
-    };
-
-    // Get all the elements you want to show on scroll
-    const targets = document.querySelectorAll(".js-show-on-scroll");
-
-    // Set up a new observer
-    const observer = new IntersectionObserver(callback);
-
-    // Loop through each of the target
-    targets.forEach(function (target) {
-      // Hide the element
-      target.classList.add("opacity-0");
-
-      // Add the element to the watcher
-      observer.observe(target);
-    });
-  }, []);
   return (
     <div className="flex flex-wrap p-10 mx-auto max-w-6xl">
       {releases.map((release) => {
-        return (
-          <div className="js-show-on-scroll">
-            <iframe
-              style={{ border: 0, width: "350px", height: "350px" }}
-              src={release.src}
-              seamless
-            >
-              <a href={release.link}>{release.title}</a>
-            </iframe>
-          </div>
-        );
+        return <MusicElement release={release} />;
       })}
     </div>
   );
