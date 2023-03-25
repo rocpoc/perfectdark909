f4e7654cb83170655f2bbe977d96b20dc9430566
rocky lubbers <rocky.lubbers@gmail.com>
2023-03-24 18:20:21 -0700

    Update Discography.tsx

--- src/components/Discography.tsx
+++ src/components/Discography.tsx
@@ -3,6 +3,11 @@ import MusicElement from "./MusicElement";
 const releases = [
   {
     title: "Pain Reinforcement",
+    src: "https://bandcamp.com/EmbeddedPlayer/album=1180745057/size=large/bgcol=333333/linkcol=ffffff/minimal=true/transparent=true/",
+    link: "https://perfectdark909.bandcamp.com/album/forgotten-places-ep",
+  },
+  {
+    title: "Pain Reinforcement",
     src: "https://bandcamp.com/EmbeddedPlayer/album=2815161335/size=large/bgcol=333333/linkcol=ffffff/minimal=true/transparent=true/",
     link: "https://perfectdark909.bandcamp.com/album/pain-reinforcement",
   },
