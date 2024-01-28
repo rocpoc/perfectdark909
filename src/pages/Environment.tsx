ab20de26e93213b8a32b55b05b90c2e5da6c4d4b
Sean <sean.lubbers@gmail.com>
2024-01-27 16:38:09 -0800

    Added verbiage to env. page (Rave for a Reason, Earth First)

--- src/pages/Environment.tsx
+++ src/pages/Environment.tsx
@@ -7,44 +7,53 @@ export const Environment: React.FC<{}> = () => {
   return (
     <Container showToolbar={true}>
       <div className="flex flex-col justify-center max-w-lg m-auto">
-        {/* <div className="text-4xl font-bold">ABOUT US</div> */}
         <div className="px-11 grow flex justify-center gap-1">
           <span className="text-4xl font-bold can-hover:hover:bg-violet-600">
             ENVIRONMENT
           </span>
         </div>
         <div className="text-xl p-8">
-          Perfect Dark is pretty down with the environment.
-          <br></br>
+          At Perfect Dark, our passion for techno is paralleled by our
+          dedication to environmental stewardship. Through our "Tracks for
+          Trees" initiative, we plant a tree for every track purchased on select
+          Bandcamp Friday. It's a direct way for our fans to engage in
+          environmental action—turning their music collection into a growing
+          forest.
+          <br />
+          <br />
+          We're also committed to local causes like Stop Valley’s Edge, actively
+          opposing unsustainable development through benefit events like{" "}
+          <em>EARTH FIRST</em>. Join us on February 10th at Duffy's in Chico,
+          where 40% of earnings will support sustainable housing solutions. By
+          attending, you're not just part of an event; you're part of the change
+          towards a more sustainable community.
+          <br />
+          <br />
+          Every ticket, every track, contributes to a greener tomorrow. Join
+          Perfect Dark in making a difference. #RaveForAReason
         </div>
         <div className="flex justify-center py-4 inset-x-0 bottom-4 max-w-lg m-auto">
-          <div>
-            <img
-              src={pd_heart_logo}
-              alt="Logo"
-              className="max-w-lg m-auto"
-              width="50"
-              height="50"
-            />
-          </div>
-          <div>
-            <img
-              src={pd_90_logo}
-              alt="Logo"
-              className="max-w-lg m-auto"
-              width="50"
-              height="50 "
-            />
-          </div>
-          <div>
-            <img
-              src={pd_spiral_logo}
-              alt="Logo"
-              className="max-w-lg m-auto"
-              width="50"
-              height="50"
-            />
-          </div>
+          <img
+            src={pd_heart_logo}
+            alt="Perfect Dark Special Heart Logo"
+            className="max-w-lg m-auto"
+            width="50"
+            height="50"
+          />
+          <img
+            src={pd_90_logo}
+            alt="Perfect Dark 90's Type Logo"
+            className="max-w-lg m-auto"
+            width="50"
+            height="50 "
+          />
+          <img
+            src={pd_spiral_logo}
+            alt="Perfect Dark Spiral Logo"
+            className="max-w-lg m-auto"
+            width="50"
+            height="50"
+          />
         </div>
       </div>
     </Container>
