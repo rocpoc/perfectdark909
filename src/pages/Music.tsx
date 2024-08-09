923a102c062c53d98ca77a3bee179495d3a07288
rocky lubbers <rocky.lubbers@gmail.com>
2024-08-08 22:20:31 -0700

    Update Music.tsx

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -5,8 +5,8 @@ import "../App.css";
 export const Music: React.FC<{}> = () => {
   return (
     <Container showToolbar={true}>
-      <div className="px-11 grow flex justify-center gap-1">
-        <span className="text-3xl font-bold can-hover:hover:text-emerald-300">
+      <div className="flex flex-col justify-center max-w-2xl m-auto px-3">
+        <span className="text-3xl xxs:text-3xl xs:text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl font-bold can-hover:hover:text-emerald-300">
           MUSIC
         </span>
       </div>
