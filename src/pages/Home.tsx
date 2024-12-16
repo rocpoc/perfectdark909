f3a900440de6673b03331fc37f7b34dd3d244632
rocky lubbers <rocky.lubbers@gmail.com>
2024-12-16 10:13:46 -0800

    add brick duffys

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,6 +20,12 @@ export const Home: React.FC<{}> = () => {
         </span>
         <br />
         <div className="text-2xl xxs:text-xl xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl font-bold can-hover:hover:text-emerald-300">
+          <a href="https://www.youtube.com/watch?v=78L5mKgGxnM" target="_blank">
+            Brick | Duffys, Chico
+          </a>
+        </div>
+        <br />
+        <div className="text-2xl xxs:text-xl xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl font-bold can-hover:hover:text-emerald-300">
           <a href="https://fanlink.tv/pd11" target="_blank">
             Saroc - Paralysis EP
           </a>
