c1a6b17fa1b955f281e09fe16c722d8041cbddec
Sean <sean.lubbers@gmail.com>
2025-01-11 20:50:38 -0800

    smh cloudflare

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,14 +20,14 @@ export const Home: React.FC<{}> = () => {
         </span>
         <br />
         <div className="text-2xl xxs:text-xl xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl font-bold can-hover:hover:text-emerald-300">
-          <a href="https://www.youtube.com/watch?v=78L5mKgGxnM" target="_blank">
-            Brick | Duffys, Chicoo
+          <a href="https://ra.co/events/2077095" target="_blank">
+            Perfect Dark ☆ Underground SF <br /> FEM TAKEOVER
           </a>
         </div>
         <br />
         <div className="text-2xl xxs:text-xl xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl font-bold can-hover:hover:text-emerald-300">
-          <a href="https://fanlink.tv/pd11" target="_blank">
-            Saroc - Paralysis EP
+          <a href="https://www.youtube.com/watch?v=78L5mKgGxnM" target="_blank">
+            Brick | Duffys, Chico
           </a>
         </div>
         <br />
@@ -40,6 +40,12 @@ export const Home: React.FC<{}> = () => {
         <div className="text-2xl xxs:text-xl xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl font-bold can-hover:hover:text-emerald-300">
           <a href="https://www.youtube.com/watch?v=KtDFBk1b_DM" target="_blank">
             Fauna | Duffys, Chico
+          </a>
+        </div>
+        <br />
+        <div className="text-2xl xxs:text-xl xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl font-bold can-hover:hover:text-emerald-300">
+          <a href="https://fanlink.tv/pd11" target="_blank">
+            Saroc - Paralysis EP
           </a>
         </div>
         <br />
