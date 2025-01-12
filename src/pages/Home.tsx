0752c7face96a5f56d4670cf26c59ab8d89f5f6f
Sean <sean.lubbers@gmail.com>
2025-01-11 20:36:01 -0800

    switchy switch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,12 +32,6 @@ export const Home: React.FC<{}> = () => {
         </div>
         <br />
         <div className="text-2xl xxs:text-xl xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl font-bold can-hover:hover:text-emerald-300">
-          <a href="https://fanlink.tv/pd11" target="_blank">
-            Saroc - Paralysis EP
-          </a>
-        </div>
-        <br />
-        <div className="text-2xl xxs:text-xl xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl font-bold can-hover:hover:text-emerald-300">
           <a href="https://www.youtube.com/watch?v=t-275yXiA2U" target="_blank">
             Carmine | Duffys, Chico
           </a>
@@ -46,6 +40,12 @@ export const Home: React.FC<{}> = () => {
         <div className="text-2xl xxs:text-xl xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl font-bold can-hover:hover:text-emerald-300">
           <a href="https://www.youtube.com/watch?v=KtDFBk1b_DM" target="_blank">
             Fauna | Duffys, Chico
+          </a>
+        </div>
+        <br />
+        <div className="text-2xl xxs:text-xl xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl font-bold can-hover:hover:text-emerald-300">
+          <a href="https://fanlink.tv/pd11" target="_blank">
+            Saroc - Paralysis EP
           </a>
         </div>
         <br />
