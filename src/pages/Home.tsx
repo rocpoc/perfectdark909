bceb050f2021ac6ab9c37f5eabecd164f81511f2
Sean <sean.lubbers@gmail.com>
2025-01-11 20:40:00 -0800

    bump

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,7 +33,7 @@ export const Home: React.FC<{}> = () => {
         <br />
         <div className="text-2xl xxs:text-xl xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl font-bold can-hover:hover:text-emerald-300">
           <a href="https://www.youtube.com/watch?v=t-275yXiA2U" target="_blank">
-            Carmine | Duffys, Chico
+            Carmine | Duffys, Chicoo
           </a>
         </div>
         <br />
